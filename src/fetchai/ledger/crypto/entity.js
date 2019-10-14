e73015d55aa69df6b9c271f6927be84cd4661ff9
Vijay Sharma <sharma881vijay@gmail.com>
2019-10-14 12:05:44 +0530

    Added entity class code

--- src/fetchai/ledger/crypto/entity.js
+++ src/fetchai/ledger/crypto/entity.js
@@ -1,5 +1,6 @@
 import { randomBytes } from 'crypto'
-import { secp256k1 } from 'secp256k1'
+import * as secp256k1 from 'secp256k1'
+import { ValidationError } from '../errors'
 
 /**
  * An entity is a full private/public key pair.
@@ -7,15 +8,57 @@ import { secp256k1 } from 'secp256k1'
  * @public
  * @class
  */
-class Entity {
+export class Entity {
+
 	constructor(private_key_bytes = '') {
+
 		// construct or generate the private key if one is not specified
 		if (private_key_bytes) {
-			console.log('Generating public and private key for account')
+			if (secp256k1.privateKeyVerify(private_key_bytes)) {
+				this.privKey = private_key_bytes
+				this.pubKey = secp256k1.publicKeyCreate(this.privKey, false)
+			} else {
+				throw new ValidationError(
+					'Unable to load private key from input'
+				)
+			}
 		} else {
 			do {
 				this.privKey = randomBytes(32)
+				this.pubKey = secp256k1.publicKeyCreate(this.privKey, false)
 			} while (!secp256k1.privateKeyVerify(this.privKey))
 		}
 	}
+
+	private_key() {
+		return this.privKey
+	}
+
+	private_key_hex() {
+		return this.privKey.toString('hex')
+	}
+
+	// get the public key in a uncompressed format
+	public_key() {
+		return this.pubKey
+	}
+
+	public_key_hex() {
+		return this.pubKey.toString('hex')
+	}
+
+	// sign the message
+	sign(extMsgHash) {
+		return secp256k1.sign(extMsgHash, this.privKey)
+	}
+
+	signature_hex(sigObj) {
+		return sigObj.signature.toString('hex')
+	}
+
+	// verify the signature
+	verify_signature(extMsgHash, sigObj) {
+		return secp256k1.verify(extMsgHash, sigObj.signature, this.pubKey)
+	}
+
 }
