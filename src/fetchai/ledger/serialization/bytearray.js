d90e82cf85b50ea7df91afbdad65ad1b485ccb64
Vijay Sharma <sharma881vijay@gmail.com>
2019-10-17 23:08:51 +0530

    [FIX] Minor bytearray on address serialization, use buffer.concat instead of buffer.write

--- src/fetchai/ledger/serialization/bytearray.js
+++ src/fetchai/ledger/serialization/bytearray.js
@@ -1,8 +1,9 @@
 import * as integer from './integer'
 
 const encode = async (stream, value) => {
-	await integer.encode(stream, value.length)
-	stream.write(new Buffer(value).toString())
+	// value in bytes (ascii encoded)
+	stream = await integer.encode(stream, value.length)
+	return Buffer.concat([stream, value])
 }
 
 export { encode }
