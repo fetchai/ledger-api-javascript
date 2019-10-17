c3b1fbf6c0a02b479ba375623295b7380ededb7c
Vijay Sharma <sharma881vijay@gmail.com>
2019-10-17 23:09:30 +0530

    [FIX] Minor integer on identity serialization, use buffer.concat instead of buffer.write

--- src/fetchai/ledger/serialization/identity.js
+++ src/fetchai/ledger/serialization/identity.js
@@ -5,8 +5,9 @@ const UNCOMPRESSED_SCEP256K1_PUBLIC_KEY = 0x04
 // const UNCOMPRESSED_SCEP256K1_PUBLIC_KEY_LEN = 64
 
 const encode = async(stream, value) => {
-	stream.write(new Buffer([UNCOMPRESSED_SCEP256K1_PUBLIC_KEY]).toString())
-	stream.write(new Buffer(value).toString())
+	// value in bytes
+	stream = Buffer.concat([stream, new Buffer([UNCOMPRESSED_SCEP256K1_PUBLIC_KEY])])
+	return Buffer.concat([stream, value])
 }
 
 export {encode}
