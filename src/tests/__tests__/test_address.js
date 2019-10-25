f21ac679780587d7f05758aed6a4ca40f1259b56
Vijay Sharma <sharma881vijay@gmail.com>
2019-10-25 13:40:59 +0530

    Linting fixes and eslint disable for no-undef from address class testcases

--- src/tests/__tests__/test_address.js
+++ src/tests/__tests__/test_address.js
@@ -1,14 +1,16 @@
-import {Address} from '../../fetchai/ledger/crypto/address'
+/* eslint-disable no-undef */
+import { Address } from '../../fetchai/ledger/crypto/address'
 import * as bs58 from 'bs58'
-import {ValidationError} from '../../fetchai/ledger/errors'
+import { ValidationError } from '../../fetchai/ledger/errors'
 
 describe(':Address', () => {
-
 	test('test construction from string', () => {
 		// generate a random 32 byte buffer
 		const digest = _calc_digest(Buffer.from('rand'))
 		const bs58_encoded = bs58.encode(digest)
-		const [expected_address_bytes, expected_display] = _calc_address(bs58_encoded)
+		const [expected_address_bytes, expected_display] = _calc_address(
+			bs58_encoded
+		)
 		const address = new Address(expected_display)
 		expect(address.toBytes()).toMatchObject(expected_address_bytes)
 		expect(address.toString()).toBe(expected_display)
@@ -17,7 +19,7 @@ describe(':Address', () => {
 	test('test construction from bytes', () => {
 		const digest = _calc_digest(Buffer.from('rand'))
 		const bs58_encoded = bs58.encode(digest)
-		const [expected_address_bytes,] = _calc_address(bs58_encoded)
+		const [expected_address_bytes] = _calc_address(bs58_encoded)
 		const address = new Address(expected_address_bytes)
 		expect(address.toBytes()).toMatchObject(expected_address_bytes)
 	})
@@ -25,7 +27,7 @@ describe(':Address', () => {
 	test('test construction from address', () => {
 		const digest = _calc_digest(Buffer.from('rand'))
 		const bs58_encoded = bs58.encode(digest)
-		const [expected_address_bytes,] = _calc_address(bs58_encoded)
+		const [expected_address_bytes] = _calc_address(bs58_encoded)
 		const address1 = new Address(expected_address_bytes)
 		const address2 = new Address(address1)
 		expect(address2.toBytes()).toMatchObject(expected_address_bytes)
@@ -56,7 +58,7 @@ describe(':Address', () => {
 	test('test hex display', () => {
 		const digest = _calc_digest(Buffer.from('rand'))
 		const bs58_encoded = bs58.encode(digest)
-		const [expected_address_bytes,] = _calc_address(bs58_encoded)
+		const [expected_address_bytes] = _calc_address(bs58_encoded)
 		const address = new Address(expected_address_bytes)
 		const address_bytes = address.toBytes()
 		const hex_address = address_bytes.toString('hex')
@@ -76,6 +78,5 @@ describe(':Address', () => {
 		expect(() => {
 			new Address(Buffer.from(bs58invalid))
 		}).toThrow(ValidationError)
-
 	})
 })
