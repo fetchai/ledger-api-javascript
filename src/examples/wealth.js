e68eedc0a70496d2aa4de627ffd6bb0f25a3f8f1
Vijay Sharma <sharma881vijay@gmail.com>
2019-10-25 14:07:22 +0530

    Updated wealth example, removed extra comments and hardcoded value

--- src/examples/wealth.js
+++ src/examples/wealth.js
@@ -5,25 +5,18 @@ import { logger } from '../fetchai/ledger/utils'
 async function main() {
 	const host = '127.0.0.1'
 	const port = 8000
+	// create the Token APIs
 	const api = new TokenApi(host, port)
 
-	let balance = await api.balance('29nQnTssh1Fe6zJtYvLfmjHqcKx5VAd5e88QpAREPvgbKUQpYw')
+	// generate a random identity
+	const identity1 = new Entity()
+	let balance = await api.balance(identity1)
 	logger.info(`Balance before wealth: ${balance}`)
 
-	const entity = new Entity(
-		new Buffer(
-			'2ff324b9d3367b160069ec67260959b4955ab519426603b5e59d5990128163f3',
-			'hex'
-		)
-	)
-	await api.wealth(entity, 1000)
+	await api.wealth(identity1, 1000)
 
-	balance = await api.balance('29nQnTssh1Fe6zJtYvLfmjHqcKx5VAd5e88QpAREPvgbKUQpYw')
+	balance = await api.balance(identity1)
 	logger.info(`Balance after wealth: ${balance}`)
 }
 
 main()
-
-// Note: use this for testing purpose
-// const entity = new Entity(new Buffer('2ff324b9d3367b160069ec67260959b4955ab519426603b5e59d5990128163f3', 'hex'))
-// hex of address: 97a389875d9ff2db65f464cd825bf8be59d3cc1e6b42cdc52e1c0476ae320c4d
