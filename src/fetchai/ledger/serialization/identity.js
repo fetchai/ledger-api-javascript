7baeb0007b2b07a5850ec252e4dec806cfae5dcf
Vijay Sharma <sharma881vijay@gmail.com>
2019-10-25 12:53:22 +0530

    Linting fixes and import missing ValidationError function

--- src/fetchai/ledger/serialization/identity.js
+++ src/fetchai/ledger/serialization/identity.js
@@ -1,30 +1,40 @@
-import {Identity} from '../crypto/identity.js'
+import { Identity } from '../crypto/identity.js'
+import { ValidationError } from '../errors'
+
 // *******************************
 // ********** Constants **********
 // *******************************
 const UNCOMPRESSED_SCEP256K1_PUBLIC_KEY = 0x04
 //*** IMPORTANT ****/////
-const UNCOMPRESSED_SCEP256K1_PUBLIC_KEY_LEN = 64;
+const UNCOMPRESSED_SCEP256K1_PUBLIC_KEY_LEN = 64
 
 const encode = (buffer, value) => {
-    if (value instanceof Identity) {
-        return Buffer.concat([buffer, new Buffer([UNCOMPRESSED_SCEP256K1_PUBLIC_KEY]), Identity.public_key_bytes])
-    } else {
-        return Buffer.concat([buffer, new Buffer([UNCOMPRESSED_SCEP256K1_PUBLIC_KEY]), value])
-    }
+	if (value instanceof Identity) {
+		return Buffer.concat([
+			buffer,
+			new Buffer([UNCOMPRESSED_SCEP256K1_PUBLIC_KEY]),
+			Identity.public_key_bytes
+		])
+	} else {
+		return Buffer.concat([
+			buffer,
+			new Buffer([UNCOMPRESSED_SCEP256K1_PUBLIC_KEY]),
+			value
+		])
+	}
 }
 
-const decode = (buffer) => {
-    const header = buffer.slice(0, 1);
-    const hex = parseInt(header.toString('hex'));
+const decode = buffer => {
+	const header = buffer.slice(0, 1)
+	const hex = parseInt(header.toString('hex'))
 
-    if (hex !== UNCOMPRESSED_SCEP256K1_PUBLIC_KEY) {
-        throw new ValidationError('Unsupported identity type');
-    }
-    // we add one to this value because our key is longer by one, and
-    // one because we start our slice ignoring the first.
-    const len = UNCOMPRESSED_SCEP256K1_PUBLIC_KEY_LEN + 2;
-    return buffer.slice(1, len);
+	if (hex !== UNCOMPRESSED_SCEP256K1_PUBLIC_KEY) {
+		throw new ValidationError('Unsupported identity type')
+	}
+	// we add one to this value because our key is longer by one, and
+	// one because we start our slice ignoring the first.
+	const len = UNCOMPRESSED_SCEP256K1_PUBLIC_KEY_LEN + 2
+	return buffer.slice(1, len)
 }
 
-export {encode, decode}
+export { encode, decode }
