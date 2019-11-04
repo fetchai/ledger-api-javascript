54221e4b9b8a1546ae748071224ee96d8a84b24a
Vijay Sharma <sharma881vijay@gmail.com>
2019-11-04 13:11:09 +0530

    Added code for unhandled rejections error/warning, added info/error logger and removed hardcoded value of address from address class object

--- src/fetchai/ledger/api/token.js
+++ src/fetchai/ledger/api/token.js
@@ -3,6 +3,7 @@ import {logger} from '../utils'
 import {ApiEndpoint} from './common'
 import {BitVector} from '../bitvector'
 import {encode_transaction} from '../serialization/transaction'
+import { Address } from '../crypto/address'
 
 /**
  * This class for all tokens operations
@@ -27,14 +28,16 @@ export class TokenApi extends ApiEndpoint {
      * @param  {address} The base64 encoded string containing the address of the node
      */
     async balance(address) {
-        logger.info(`request for check balance of address: ${address}`)
 
         // format and make the request
-        let request = {address: String(address)}
+        address =  new Address(address.privKey)
+        let request = { address: address._display }
+        logger.info(`request for check balance of address: ${address._display}`)
         let data = await super._post_json('balance', request, this.prefix)
         logger.info(`Balance of ${address} is ${data.balance}`)
 
         if (!('balance' in data)) {
+            logger.error('No response data from server.')
             throw new ApiError('Malformed response from server')
         }
 
@@ -55,25 +58,30 @@ export class TokenApi extends ApiEndpoint {
             `request for creating wealth of address ${entity.public_key_hex()} for amount ${amount}`
         )
 
+        let address =  new Address(entity.privKey)
         // wildcard for the moment
         let shard_mask = new BitVector()
         let tx = await super.create_skeleton_tx(1)
-        // Todo: Replace entity.pubKey with hex of address
-        // Note: use 97a389875d9ff2db65f464cd825bf8be59d3cc1e6b42cdc52e1c0476ae320c4d for testing
-        tx.from_address(entity.public_key_hex()) //hex of address
+        tx.from_address(address._address) //hex of address
         tx.target_chain_code(this.API_PREFIX, shard_mask)
         tx.action = 'wealth'
         tx.add_signer(entity.public_key_hex()) // hex of public key
 
         // format the transaction payload
         tx.data = super._encode_json({
-            address: entity.public_key(), //base64 encoded public key
+            address: entity.pubKey.toString('base64'), //base64 encoded public key
             amount: amount
         })
-        logger.info(`Transactions object for sign and encode: ${JSON.stringify(tx, null, '\t')}`)
+        logger.info(
+            `Transactions object for sign and encode: ${JSON.stringify(
+                tx,
+                null,
+                '\t'
+            )}`
+        )
 
         // encode and sign the transaction
-        const encoded_tx = await encode_transaction(tx, entity)
+        const encoded_tx = encode_transaction(tx, entity)
 
         // submit the transaction
         return await this._post_tx_json(encoded_tx, 'wealth')
@@ -95,25 +103,35 @@ export class TokenApi extends ApiEndpoint {
             `request for transferring ${amount} wealth from ${entity.public_key_hex()} to ${to} with fee ${fee}`
         )
 
+        let address =  new Address(entity.privKey)
         // build up the basic transaction information
         let tx = await super.create_skeleton_tx(fee)
-        // Todo: Replace entity.pubKey with hex of address
-        // Note: use 97a389875d9ff2db65f464cd825bf8be59d3cc1e6b42cdc52e1c0476ae320c4d for testing
-        tx.from_address(entity.public_key_hex()) //hex of address
+        tx.from_address(address._address) //hex of address
         tx.add_transfer(to, amount)
         tx.add_signer(entity.public_key_hex()) // hex of public key
 
         // format the transaction payload
         tx.data = super._encode_json({
-            address: entity.public_key(), //base64 encoded public key
+            address: entity.pubKey.toString('base64'), //base64 encoded public key
             amount: amount
         })
-        logger.info(`Transactions object for sign and encode: ${JSON.stringify(tx, null, '\t')}`)
+        logger.info(
+            `Transactions object for sign and encode: ${JSON.stringify(
+                tx,
+                null,
+                '\t'
+            )}`
+        )
 
         // encode and sign the transaction
-        const encoded_tx = await encode_transaction(tx, entity)
+        const encoded_tx = encode_transaction(tx, entity)
 
         // submit the transaction
         return await this._post_tx_json(encoded_tx, 'transfer')
     }
 }
+
+// Important
+process.on('unhandledRejection', reason => {
+    console.log('Error details:', reason || reason.stack)
+})
