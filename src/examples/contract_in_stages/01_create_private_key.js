6a720f9064ec1c0de9cd52ee09786fc69eefe5c0
Vijay Sharma <sharma881vijay@gmail.com>
2019-12-06 16:41:00 +0530

    Updated 01_create_private_key.js example

--- src/examples/contract_in_stages/01_create_private_key.js
+++ src/examples/contract_in_stages/01_create_private_key.js
@@ -5,7 +5,7 @@ async function main() {
     console.log('Creating private key...')
     const entity = new Entity()
     let writeStream = fs.createWriteStream('src/examples/contract_in_stages/private.key')
-    const data = entity.prompt_dump('src/examples/contract_in_stages/private.key')
+    const data = entity.prompt_dump('src/examples/contract_in_stages/private.key', '1234567890qwertyuiopQWERTYUIOP!@#$')
     writeStream.write(data)
     writeStream.end()
     console.log('Creating private key...complete')
