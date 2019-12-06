e52749415c2699af1e2dbe2cb4c1f5d7f770c61c
Ed FitzGerald <40318762+ejfitzgerald@users.noreply.github.com>
2019-12-06 12:42:42 +0000

    Add production configuration for vanila javascript bundle (#47)

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,36 @@ module.exports = [
         output: {
             path: __dirname + '/bundle',
             publicPath: '/bundle/',
-            filename: 'vanilla.js',
+            filename: 'fetchai-ledger-api.js',
+            library: 'fetchai',
+            libraryTarget: 'var'
+        },
+        devServer: {
+            contentBase: './bundle'
+        }
+    },
+    {
+        mode: 'production',
+        entry: './src/fetchai/ledger/index.js',
+        node: {
+            fs: 'empty',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader']
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js']
+        },
+        output: {
+            path: __dirname + '/bundle',
+            publicPath: '/bundle/',
+            filename: 'fetchai-ledger-api.min.js',
             library: 'fetchai',
             libraryTarget: 'var'
         },
