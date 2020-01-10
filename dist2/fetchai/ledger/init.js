a8f8493dc35f5e7cea66de41358d1fa1acc29ff3
dougb99 <douglaswburkinshaw@hotmail.co.uk>
2020-01-10 01:31:06 +0000

    w

--- dist2/fetchai/ledger/init.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Ledger version that this API is compatible with
-var __version__ = '0.9.0-a1';
-exports.__version__ = __version__;
-// This API is compatible with ledgers that meet all the requirements listed here:
-var __compatible__ = ['<0.10.0', '>=0.8.0'];
-exports.__compatible__ = __compatible__;
-//# sourceMappingURL=init.js.map
